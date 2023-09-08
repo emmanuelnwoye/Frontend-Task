4002fac9bc452e80eb45fbc186d86a44f13efbce
Emmanuel Nwoye <111285226+emmanuelnwoye@users.noreply.github.com>
2023-09-08 03:57:03 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
 document.addEventListener('DOMContentLoaded', function() {
+    function updateClock() {
+        const currentDate = new Date(Date.now() - (11 * 60 * 60 * 1000));
+        let hours = (currentDate.getUTCHours() + 11) % 12 + 1;
+        const minutes = currentDate.getUTCMinutes();
+        const seconds = currentDate.getUTCSeconds();
+        const milliseconds = currentDate.getUTCMilliseconds();
+        const ampm = hours >= 12 ? 'PM' : 'AM';
+        hours = hours % 12 || 12;
 
-    function getCurrentDayOfWeek() {
-        const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-        const currentDate = new Date();
-        const dayIndex = currentDate.getUTCDay();
-        return daysOfWeek[dayIndex];
-    }
+        document.getElementById('hour').textContent = hours.toString().padStart(2, '0');
+        document.getElementById('minutes').textContent = minutes.toString().padStart(2, '0');
+        document.getElementById('seconds').textContent = seconds.toString().padStart(2, '0');
+        document.getElementById('milliseconds').textContent = milliseconds.toString().padStart(3, '0');
+        document.getElementById('ampm').textContent = ampm;
+}
 
-    function getCurrentUTCTime() {
-        const currentDate = new Date();
-        const hours = currentDate.getUTCHours().toString().padStart(2, '0');
-        const Minutes = currentDate.getUTCMinutes().toString().padStart(2, '0');
-        return '${hours}:${minutes} UTC';
-    }
+    setInterval(updateClock, 1);
 
-    document.querySelector('[data-testid="currentDayOfTheWeek"]').textContent = getCurrentDayOfWeek();
-    document.querySelector('[data-testid="currentUTCTime"]').textContent = getCurrentUTCTime();
+    const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    const currentDate = new Date();
+    const dayIndex = currentDate.getUTCDay();
+    const currentDay = daysOfWeek[dayIndex];
+    document.querySelector('[data-testid="currentDayOfTheWeek"]').textContent = currentDay();
 });
